ed0756abd3d799ca4b2ba8c78d566156e7c02dbe
Om <shahom530@gmail.com>
2026-04-02 13:51:11 +0530

    Save latest search implementation

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -1,18 +1,8 @@
 'use client';
 import { useCallback, useRef } from 'react';
 
-import { useCallback, useEffect, useRef } from 'react';
-
 export function useDebounce<T extends (...args: any[]) => any>(callback: T, delay: number) {
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-
-  useEffect(() => {
-    return () => {
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
-      }
-    };
-  }, []);
 
   const debouncedCallback = useCallback(
     (...args: Parameters<T>) => {
